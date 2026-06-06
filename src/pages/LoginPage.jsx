d7f6e6d7d7a86f74e00daa425fd84c0ba8f86764
Abdur Rahman Rasel <mdraselahmed063@gmail.com>
2026-06-06 23:20:20 +0600

    login disable button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,37 @@
 import { Link } from "react-router";
 import InputField from "../components/ui/InputField";
 import Button from "../components/ui/Button";
+import {useAuthStore} from "../store/useAuthStore";
 
 export default function LoginPage() {
+
+  const {loading, login} = useAuthStore();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    login();
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="w-100 bg-white p-6 rounded-lg shadow-md">
           <h2 className="text-3xl font-medium mb-4">Login</h2>
 
-          <form>
-              <InputField
-              label="Full Name" 
-              type="text" 
-              placeholder="Enter your Full Name" />
+          <form onSubmit={submitHandler}>
+              
               <InputField
               label="Enter Email Address" 
               type="email" 
               placeholder="Enter your Email Address" />
-
+              <InputField
+              label="Password" 
+              type="password" 
+              placeholder="Enter your Password" />
               <p>
                 <input type="checkbox" id="remember" className="mr-2" />
                 <label htmlFor="remember">Remember me</label>
               </p>
-            <Button type="submit">Login</Button>
+            <Button type="submit" loading={loading}>Login</Button>
               <p className="text-sm pt-4 inline-block">
                 Don't have an account? <Link to="/register" className="text-blue-500">Register Now</Link>
               </p>
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,12 @@
+import { create } from "zustand"
+
+
+export const useAuthStore = create((set, get) => ({
+    loading: false,
+    login: () => {
+        set({loading: true})
+        setTimeout(() => {
+            set({loading: false})
+        }, 2500)
+    }
+}))
